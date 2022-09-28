d83a146eee3923071d0346d2d14c80529408689f
Ingoo Kang <47412229+kig2929kig@users.noreply.github.com>
2022-09-28 12:18:37 +0900

    Add files via upload

--- abc.py
+++ abc.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 # pip install selenium
 # pip install webdriver_manager
 
@@ -15,9 +16,10 @@ option.add_experimental_option('excludeSwitches', ['enable-logging'])
 chrome_driver = Service(ChromeDriverManager().install())
 #chrome_driver = Service(ChromeDriverManager(path="경로명").install())
 driver = webdriver.Chrome(service = chrome_driver , options=option)
-driver.get('https://google.co.kr')
+driver.get('https://mail.google.com/mail/&ogbl')
 
-driver.find_element(By.CSS_SELECTOR, ".gLFyf.gsfi").send_keys("뽀로로")
-driver.find_element(By.CSS_SELECTOR, ".gLFyf.gsfi").send_keys(Keys.ENTER)
-#gLFyf gsfi
-#q
+driver.find_element(By.CSS_SELECTOR, ".whsOnd.zHQkBf").send_keys("ansanths.computer@gmail.com")
+driver.find_element(By.CSS_SELECTOR, ".VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.qIypjc.TrZEUc.lw1w4b").click()
+time.sleep(5)
+driver.find_element(By.CSS_SELECTOR, ".whsOnd.zHQkBf").send_keys("ansan8955")
+driver.find_element(By.CSS_SELECTOR, ".VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.qIypjc.TrZEUc.lw1w4b").click()
