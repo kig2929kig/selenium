2cc99c6a40cd33d870ebd0ba57a0ca0559763eba
Ingoo Kang <47412229+kig2929kig@users.noreply.github.com>
2022-09-28 12:29:55 +0900

    Add files via upload

--- abc.py
+++ abc.py
@@ -8,7 +8,6 @@ import time
 # pip install selenium
 # pip install webdriver_manager
 
-
 option = Options()
 option.add_experimental_option('detach', True)
 option.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -23,3 +22,19 @@ driver.find_element(By.CSS_SELECTOR, ".VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe
 time.sleep(5)
 driver.find_element(By.CSS_SELECTOR, ".whsOnd.zHQkBf").send_keys("ansan8955")
 driver.find_element(By.CSS_SELECTOR, ".VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.qIypjc.TrZEUc.lw1w4b").click()
+time.sleep(5)
+driver.find_element(By.CSS_SELECTOR, ".T-I.T-I-KE.L3").click()
+time.sleep(3)
+driver.find_element(By.CSS_SELECTOR, ".agP.aFw").send_keys("ansanths.computer@gmail.com")
+driver.find_element(By.CSS_SELECTOR, ".agP.aFw").send_keys(Keys.TAB)
+driver.find_element(By.CSS_SELECTOR, ".aoT").send_keys("테스트 메일입니다.")
+driver.find_element(By.CSS_SELECTOR, ".agP.aFw").send_keys(Keys.TAB)
+time.sleep(3)
+driver.find_element(By.CSS_SELECTOR, ".Am.Al.editable.LW-avf.tS-tW").send_keys("안녕")
+driver.find_element(By.CSS_SELECTOR, ".Am.Al.editable.LW-avf.tS-tW").send_keys(Keys.ENTER)
+driver.find_element(By.CSS_SELECTOR, ".Am.Al.editable.LW-avf.tS-tW").send_keys("좋은 하루 보내세요.")
+driver.find_element(By.CSS_SELECTOR, ".T-I.J-J5-Ji.aoO.v7.T-I-atl.L3").click()
+
+
+
+
